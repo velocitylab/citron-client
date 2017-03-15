ed6794a5a339da37de896eb7d6aa991b65a9dc29
jeonkuk <MACAIR@jeonkuk-ui-MacBook-Air.local>
2017-03-16 00:24:05 +0900

    초기 로딩 페이지 및 main page 추가

--- app/src/main/java/cityon/velo/com/cityon/LoadingActivity.java
+++ app/src/main/java/cityon/velo/com/cityon/LoadingActivity.java
@@ -1,13 +1,34 @@
 package cityon.velo.com.cityon;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 
-public class LoadingActivity extends AppCompatActivity {
+import cityon.velo.com.cityon.activity.MainActivity;
+
+public class LoadingActivity extends Activity {
+
+    //private static final String TYPEFACE_NAME = "BM-JUA.ttf";
+    //private Typeface typeface = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+
+        Handler handler = new Handler(){
+            public void handleMessage(Message msg){
+                super.handleMessage(msg);
+                //Intent intent = new Intent(getApplicationContext(),ConfigActivity.class);
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.abc_fade_in, R.anim.abc_fade_out);
+                finish();
+            }
+        };
+        handler.sendEmptyMessageDelayed(0, 500);
     }
 }
