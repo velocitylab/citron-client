cd83b9f1466c7b397c7f2ebd4008ca8d401bbb21
jeonkuk <MACAIR@jeonkuk-ui-MacBook-Air.local>
2017-03-16 00:03:30 +0900

    reinit_2

---
+++ app/src/main/java/cityon/velo/com/cityon/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package cityon.velo.com.cityon.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cityon.velo.com.cityon.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
